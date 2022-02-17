d918ff481af7b3a99dc4c1fe952152af026ce121
donggune kim <kdg96g@gmail.com>
2022-02-18 03:03:32 +0900

    feat: add skill page
    svg를 이용하여 기술을 능력치 느낌으로 표현

--- src/components/pages/Skill.tsx
+++ src/components/pages/Skill.tsx
@@ -1,15 +1,20 @@
-import React, { useRef, useEffect } from "react";
+import React from "react";
 import styled from '@emotion/styled';
+import { keyframes } from "@emotion/react";
+import { css } from "@emotion/css";
+import { FrontStatus, LibraryNBackStatus } from "../svg/Status";
 
 const Skill = () => {
   return (
     <Conatiner>
-      <H1>
-        
-      </H1>
-      <Title>
-        react, typescript, redux-toolkit, javascript, python, mongodb, nodejs, next, getsby
-      </Title>
+      <div>
+        <Title>Front skill</Title>
+        <FrontStatus />
+      </div>
+      <div>
+        <Title>library & Back skill</Title>
+        <LibraryNBackStatus />
+      </div>
     </Conatiner>
   );
 }
@@ -18,21 +23,44 @@ export default React.memo(Skill);
 
 const Conatiner = styled.div`
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
-`;
-
-const H1 = styled.h1`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 24px;
+  width: 100%;
+  height: 100%;
+  & > div:first-of-type {
+    margin-right: 36px;
+  }
 `;
 
 const Title = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: 44px;
+  font-size: 32px;
+  font-weight: 600;
+  color: #00b841;
+`;
+
+const linePlay = keyframes`
+  0% {
+    stroke-dashoffset: 800;
+  }
+  10% {
+    stroke-dashoffset: 0;
+  }
+  70% {
+    stroke-dashoffset: 0;
+  }
+  90% {
+    stroke-dashoffset: -800;  
+  }
+  100% {
+    stroke-dashoffset: -800;
+  }
+`
+
+const toolLine = css`
+stroke-dasharray: 800;
+stroke-dashoffset: 800;
+animation: ${linePlay} 6s ease-out infinite forwards;
 `;
---
+++ src/components/svg/Status.tsx
@@ -0,0 +1,99 @@
+import React from "react";
+import { keyframes } from "@emotion/react";
+import { css } from "@emotion/css";
+
+export const FrontStatus = () => {
+  return (
+    <svg className="design-tool" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 458.57 423" width={500}>
+      <g>
+        <g transform="translate(39.910793 14.0849)">
+          <polygon points="-27.710626,-64 0.004374,-48 0.004374,-16 -27.710626,0 -55.425626,-16 -55.425626,-48 -27.710626,-64" transform="translate(216.369834 237.915101)" fill="rgba(0,0,0,0)" stroke="#fff" stroke-dashoffset="10" stroke-dasharray="4" />
+          <polygon points="0,-64 55.425626,-32 55.425626,32 0,64 -55.425626,32 -55.425626,-32 0,-64" transform="translate(188.659208 205.915101)" fill="rgba(0,0,0,0)" stroke="#fff" stroke-dasharray="4" />
+          <polygon points="27.714374,-64 110.854374,-16 110.854374,80 27.714374,128 -55.425626,80 -55.425626,-16 27.714374,-64" transform="translate(160.944834 173.915101)" fill="rgba(0,0,0,0)" stroke="#fff" stroke-dasharray="4" />
+          <polygon points="55.429374,-64 166.284374,0 166.284374,128 55.429374,192 -55.425626,128 -55.425626,0 55.429374,-64" transform="translate(133.229833 141.915101)" fill="rgba(0,0,0,0)" stroke="#fff" stroke-dasharray="4" />
+          <polygon points="83.144374,-64 221.714374,16 221.714374,176 83.144374,256 -55.425626,176 -55.425626,16 83.144374,-64" transform="translate(105.514835 109.915101)" fill="rgba(0,0,0,0)" stroke="#fff" stroke-dashoffset="10" stroke-dasharray="0" />
+          <text dx="0" dy="0" font-size="18" font-weight="400" transform="translate(204.26351 212.156018)" fill="#fff" stroke-width="0"><tspan y="0" font-weight="400" stroke-width="0">1</tspan></text>
+          <text dx="0" dy="0" font-size="18" font-weight="400" transform="translate(231.974136 212.166775)" fill="#fff" stroke-width="0"><tspan y="0" font-weight="400" stroke-width="0">2</tspan></text>
+          <text dx="0" dy="0" font-size="18" font-weight="400" transform="translate(259.68851 212.156018)" fill="#fff" stroke-width="0"><tspan y="0" font-weight="400" stroke-width="0">3</tspan></text>
+          <text dx="0" dy="0" font-size="18" font-weight="400" transform="translate(287.403509 212.156018)" fill="#fff" stroke-width="0"><tspan y="0" font-weight="400" stroke-width="0">4</tspan></text>
+          <text dx="0" dy="0" font-size="18" font-weight="400" transform="translate(315.118511 212.433888)" fill="#fff" stroke-width="0"><tspan y="0" font-weight="400" stroke-width="0">5</tspan></text>
+          <path d="M138.570006,192q0,128,0,128" transform="translate(50.089201 45.915101)" fill="none" stroke="#fff" stroke-width="1.28" stroke-dasharray="4" />
+          <path d="M166.285007,176l110.855001,64" transform="translate(50.089201 45.915101)" fill="none" stroke="#fff" stroke-width="1.28" stroke-dasharray="4" />
+          <path d="M166.285007,144L277.140008,80" transform="translate(50.089201 45.915101)" fill="none" stroke="#fff" stroke-width="1.28" stroke-dasharray="4" />
+          <path d="M138.570008,0l-.000002,128" transform="translate(50.089201 45.915101)" fill="none" stroke="#fff" stroke-width="1.28" stroke-dasharray="4" />
+          <path d="M110.855007,144L0.000008,80" transform="translate(50.089202 45.915101)" fill="none" stroke="#fff" stroke-width="1.28" stroke-dasharray="4" />
+          <path d="M110.855007,176L0.000009,240" transform="translate(50.089201 45.915101)" fill="none" stroke="#fff" stroke-width="1.28" stroke-dasharray="4" />
+        </g>
+        <g transform="translate(0 0.000001)">
+          <text dx="0" dy="0" font-size="22" font-weight="400" transform="translate(368.140002 134.748757)" fill="#fff" stroke-width="0"><tspan y="0" font-weight="400" stroke-width="0">Typescript</tspan></text>
+          <text dx="0" dy="0" font-size="22" font-weight="400" transform="translate(375.296967 321.004981)" fill="#fff" stroke-width="0"><tspan y="0" font-weight="400" stroke-width="0">React</tspan></text>
+          <text dx="0" dy="0" font-size="22" font-weight="400" transform="translate(204.779816 406.971481)" fill="#fff" stroke-width="0"><tspan y="0" font-weight="400" stroke-width="0">Next</tspan></text>
+          <text dx="0" dy="0" font-size="22" font-weight="400" transform="translate(27.728576 321.004981)" fill="#fff" stroke-width="0"><tspan y="0" font-weight="400" stroke-width="0">Redux</tspan></text>
+          <text dx="0" dy="0" font-size="22" font-weight="400" transform="translate(184.43132 30.953601)" fill="#fff" stroke-width="0"><tspan y="0" font-weight="400" stroke-width="0">Html, Css,</tspan><tspan x="0" y="22" font-weight="400" stroke-width="0">Javascript</tspan></text>
+          <text dx="0" dy="0" font-size="22" font-weight="400" transform="translate(14.27388 134.748757)" fill="#fff" stroke-width="0"><tspan y="0" font-weight="400" stroke-width="0">Gatsby</tspan></text>
+        </g>
+        <path className={toolLine} d="M228.570002,92.000001l110.854998,64v128l-110.854998,64-110.855002-64l83.140001-80l27.715001-112Z" fill="none" stroke="#00b841" stroke-width="2.28" />
+      </g>
+    </svg>
+  );
+}
+
+export const LibraryNBackStatus = () => {
+  return (
+    <svg className="design-tool" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 458.57 423" width={500}>
+      <g>
+        <g transform="translate(39.910793 14.0849)">
+          <polygon points="-27.710626,-64 0.004374,-48 0.004374,-16 -27.710626,0 -55.425626,-16 -55.425626,-48 -27.710626,-64" transform="translate(216.369834 237.915101)" fill="rgba(0,0,0,0)" stroke="#fff" stroke-dashoffset="10" stroke-dasharray="4" />
+          <polygon points="0,-64 55.425626,-32 55.425626,32 0,64 -55.425626,32 -55.425626,-32 0,-64" transform="translate(188.659208 205.915101)" fill="rgba(0,0,0,0)" stroke="#fff" stroke-dasharray="4" />
+          <polygon points="27.714374,-64 110.854374,-16 110.854374,80 27.714374,128 -55.425626,80 -55.425626,-16 27.714374,-64" transform="translate(160.944834 173.915101)" fill="rgba(0,0,0,0)" stroke="#fff" stroke-dasharray="4" />
+          <polygon points="55.429374,-64 166.284374,0 166.284374,128 55.429374,192 -55.425626,128 -55.425626,0 55.429374,-64" transform="translate(133.229833 141.915101)" fill="rgba(0,0,0,0)" stroke="#fff" stroke-dasharray="4" />
+          <polygon points="83.144374,-64 221.714374,16 221.714374,176 83.144374,256 -55.425626,176 -55.425626,16 83.144374,-64" transform="translate(105.514835 109.915101)" fill="rgba(0,0,0,0)" stroke="#fff" stroke-dashoffset="10" stroke-dasharray="0" />
+          <text dx="0" dy="0" font-size="18" font-weight="400" transform="translate(204.26351 212.156018)" fill="#fff" stroke-width="0"><tspan y="0" font-weight="400" stroke-width="0">1</tspan></text>
+          <text dx="0" dy="0" font-size="18" font-weight="400" transform="translate(231.974136 212.166775)" fill="#fff" stroke-width="0"><tspan y="0" font-weight="400" stroke-width="0">2</tspan></text>
+          <text dx="0" dy="0" font-size="18" font-weight="400" transform="translate(259.68851 212.156018)" fill="#fff" stroke-width="0"><tspan y="0" font-weight="400" stroke-width="0">3</tspan></text>
+          <text dx="0" dy="0" font-size="18" font-weight="400" transform="translate(287.403509 212.156018)" fill="#fff" stroke-width="0"><tspan y="0" font-weight="400" stroke-width="0">4</tspan></text>
+          <text dx="0" dy="0" font-size="18" font-weight="400" transform="translate(315.118511 212.433888)" fill="#fff" stroke-width="0"><tspan y="0" font-weight="400" stroke-width="0">5</tspan></text>
+          <path d="M138.570006,192q0,128,0,128" transform="translate(50.089201 45.915101)" fill="none" stroke="#fff" stroke-width="1.28" stroke-dasharray="4" />
+          <path d="M166.285007,176l110.855001,64" transform="translate(50.089201 45.915101)" fill="none" stroke="#fff" stroke-width="1.28" stroke-dasharray="4" />
+          <path d="M166.285007,144L277.140008,80" transform="translate(50.089201 45.915101)" fill="none" stroke="#fff" stroke-width="1.28" stroke-dasharray="4" />
+          <path d="M138.570008,0l-.000002,128" transform="translate(50.089201 45.915101)" fill="none" stroke="#fff" stroke-width="1.28" stroke-dasharray="4" />
+          <path d="M110.855007,144L0.000008,80" transform="translate(50.089202 45.915101)" fill="none" stroke="#fff" stroke-width="1.28" stroke-dasharray="4" />
+          <path d="M110.855007,176L0.000009,240" transform="translate(50.089201 45.915101)" fill="none" stroke="#fff" stroke-width="1.28" stroke-dasharray="4" />
+        </g>
+        <g transform="translate(0 0.000001)">
+          <text dx="0" dy="0" font-size="22" font-weight="400" transform="translate(368.140002 134.748757)" fill="#fff" stroke-width="0"><tspan y="0" font-weight="400" stroke-width="0">Skill</tspan></text>
+          <text dx="0" dy="0" font-size="22" font-weight="400" transform="translate(375.296967 321.004981)" fill="#fff" stroke-width="0"><tspan y="0" font-weight="400" stroke-width="0">MongoDB</tspan></text>
+          <text dx="0" dy="0" font-size="22" font-weight="400" transform="translate(204.779816 406.971481)" fill="#fff" stroke-width="0"><tspan y="0" font-weight="400" stroke-width="0">Skill</tspan></text>
+          <text dx="0" dy="0" font-size="22" font-weight="400" transform="translate(27.728576 321.004981)" fill="#fff" stroke-width="0"><tspan y="0" font-weight="400" stroke-width="0">Threejs</tspan></text>
+          <text dx="0" dy="0" font-size="22" font-weight="400" transform="translate(194.43132 45.953601)" fill="#fff" stroke-width="0"><tspan y="0" font-weight="400" stroke-width="0">Nodejs</tspan></text>
+          <text dx="0" dy="0" font-size="22" font-weight="400" transform="translate(14.27388 134.748757)" fill="#fff" stroke-width="0"><tspan y="0" font-weight="400" stroke-width="0">Graphql</tspan></text>
+        </g>
+        <path className={toolLine} d="M228.570002,92.000001l110.854998,64v128l-110.854998,64-110.855002-64l83.140001-80l27.715001-112Z" fill="none" stroke="#00b841" stroke-width="2.28" />
+      </g>
+    </svg>
+  );
+}
+
+const linePlay = keyframes`
+  0% {
+    stroke-dashoffset: 800;
+  }
+  10% {
+    stroke-dashoffset: 0;
+  }
+  70% {
+    stroke-dashoffset: 0;
+  }
+  90% {
+    stroke-dashoffset: -800;  
+  }
+  100% {
+    stroke-dashoffset: -800;
+  }
+`
+
+const toolLine = css`
+stroke-dasharray: 800;
+stroke-dashoffset: 800;
+animation: ${linePlay} 6s ease-out infinite forwards;
+`;
