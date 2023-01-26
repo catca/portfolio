3dd5ae0583f473f7940aa39ef2940f43851ce121
catca <kdg96g@gmail.com>
2023-01-26 13:41:11 +0900

    fix: 프로젝트 소개에 PengCatPuzzle 추가
    BunnyMarket 제거

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,32 +1,39 @@
 import React, { Dispatch, SetStateAction, useEffect } from "react";
-import { useDispatch, useSelector } from 'react-redux';
-import { selectPage, setCurrentPage } from '../../lib/redux/page/pageSlice';
+import { useDispatch, useSelector } from "react-redux";
+import { selectPage, setCurrentPage } from "../../lib/redux/page/pageSlice";
 import styled from "@emotion/styled";
 import state from "../../lib/store";
 
 const header = [
-  { title: '소개', current: 1 },
-  { title: '기술', current: 2 },
-  { title: '프로젝트', current: 3 },
-  { title: '마침', current: 4 }
-]
+  { title: "소개", current: 1 },
+  { title: "기술", current: 2 },
+  { title: "프로젝트", current: 3 },
+  { title: "마침", current: 4 },
+];
 
 const Header: React.FC = () => {
   const { currentPage } = useSelector(selectPage);
   const dispatch = useDispatch();
   const onClick = (current: number) => {
     dispatch(setCurrentPage(current));
-  }
+  };
   return (
     <Container>
       {header.map(({ title, current }, index) => {
         return (
-          <Nav key={index} current={current} page={currentPage} onClick={() => onClick(current)}>{title}</Nav>
-        )
+          <Nav
+            key={index}
+            current={current}
+            page={currentPage}
+            onClick={() => onClick(current)}
+          >
+            {title}
+          </Nav>
+        );
       })}
     </Container>
-  )
-}
+  );
+};
 
 export default Header;
 
@@ -40,7 +47,8 @@ const Container = styled.header`
   height: 60px;
   width: 100vw;
   display: flex;
-  box-shadow: 0px 2px 4px -1px rgb(0 0 0 / 20%), 0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%);
+  box-shadow: 0px 2px 4px -1px rgb(0 0 0 / 20%),
+    0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%);
   background-color: rgba(0, 0, 0, 0);
   z-index: 12;
 `;
@@ -51,7 +59,8 @@ const Nav = styled.div<containerProps>`
   display: flex;
   justify-content: center;
   align-items: center;
-  color: #FFFFFF;
+  color: #ffffff;
   cursor: pointer;
-  border-bottom: ${(props: { page: any; current: any; }) => props.page === props.current ? 'solid 1px #FFFFFF' : 'none'};
+  border-bottom: ${(props: { page: any; current: any }) =>
+    props.page === props.current ? "solid 1px #FFFFFF" : "none"};
 `;
--- src/components/three/Frames.tsx
+++ src/components/three/Frames.tsx
@@ -1,10 +1,10 @@
-import * as THREE from 'three';
-import React, { MutableRefObject, useEffect, useRef, useState } from 'react';
-import { ThreeEvent, useFrame } from '@react-three/fiber';
-import { useCursor, Image, Text, Html } from '@react-three/drei';
-import getUuid from 'uuid-by-string';
-import { Group } from 'three';
-import styled from '@emotion/styled';
+import * as THREE from "three";
+import React, { MutableRefObject, useEffect, useRef, useState } from "react";
+import { ThreeEvent, useFrame } from "@react-three/fiber";
+import { useCursor, Image, Text, Html } from "@react-three/drei";
+import getUuid from "uuid-by-string";
+import { Group } from "three";
+import styled from "@emotion/styled";
 
 const GOLDENRATIO = 1.61803398875;
 const images = [
@@ -12,66 +12,73 @@ const images = [
   {
     position: [0, 0, 2],
     rotation: [0, 0, 0],
-    url: '/img/gallery/portfolio.PNG',
-    intro: 'portfolio',
-    href: 'https://catca-portfolio.vercel.app/',
-    git: 'https://github.com/catca/portfolio',
-    notion: 'https://gorgeous-orbit-6d2.notion.site/b9eefeb27efe4a81acf301ad2dd5e9fc',
-    img: '',
-    explanation: ` react, typescript를 기반으로 제작한 포트폴리오.\nthree.js를 사용하여 우주에 있는 듯한 느낌을 연출하였습니다.\nsvg, js를 활용하여 여러 가지 애니메이션을 주었습니다.`
-  },
-  // Left
-  {
-    position: [-2.2, 0, 2.2],
-    rotation: [0, Math.PI / 7, 0],
-    url: '/img/gallery/shiningstargram.PNG',
-    intro: 'shiningstargram',
-    href: 'https://shiningstargram.vercel.app',
-    git: 'https://github.com/intsa-fullstack/shiningstar',
-    notion: 'https://gorgeous-orbit-6d2.notion.site/shiningstargram-2021-09-2022-01-52e32beacb3a49ae85862949941fd9ces',
-    img: '/img/stargram.png',
-    explanation: ' react, next, typescript를 기반으로 제작한 SNS 웹사이트 입니다.'
+    url: "/img/gallery/PengCatPuzzle.PNG",
+    intro: "PengCatPuzzle",
+    href: "https://pengcatpuzzle.vercel.app/",
+    git: "https://github.com/penguinAndCat/puzzle",
+    notion: "",
+    img: "/img/puzzleLogo.PNG",
+    explanation: ` 직소 퍼즐을 맞출 수 있는 웹사이트입니다.\nCanvas와 Paper.js를 사용해 직소 퍼즐을 구현하였습니다.\n 소켓 통신으로 다른 사람들과 함께 맞출 수 있습니다.`,
   },
   // Right
   {
     position: [2.2, 0, 2.2],
     rotation: [0, -Math.PI / 7, 0],
-    url: '/img/gallery/bunnymarket.PNG',
-    intro: 'bunnymarket',
-    href: 'http://bunnymarket.o-r.kr:3000',
-    git: 'https://github.com/catca/bunnyMarket',
-    notion: 'https://gorgeous-orbit-6d2.notion.site/bunny-market-f49643edb7ce475a99f3f7f0a117e143',
-    img: '/img/bunnymarket.jpg',
-    explanation: ' react를 기반으로 제작한 중고마켓 플랫폼 입니다.\njavascript, spring으로 만든 졸업작품을 react로 리팩토링한 웹사이트 입니다.'
+    url: "/img/gallery/portfolio.PNG",
+    intro: "portfolio",
+    href: "https://catca-portfolio.vercel.app/",
+    git: "https://github.com/catca/portfolio",
+    notion:
+      "https://gorgeous-orbit-6d2.notion.site/b9eefeb27efe4a81acf301ad2dd5e9fc",
+    img: "",
+    explanation: ` react, typescript를 기반으로 제작한 포트폴리오.\nthree.js를 사용하여 우주에 있는 듯한 느낌을 연출하였습니다.\nsvg, js를 활용하여 여러 가지 애니메이션을 주었습니다.`,
   },
-]
+  // Left
+  {
+    position: [-2.2, 0, 2.2],
+    rotation: [0, Math.PI / 7, 0],
+    url: "/img/gallery/shiningstargram.PNG",
+    intro: "shiningstargram",
+    href: "https://shiningstargram.vercel.app",
+    git: "https://github.com/intsa-fullstack/shiningstar",
+    notion:
+      "https://gorgeous-orbit-6d2.notion.site/shiningstargram-2021-09-2022-01-52e32beacb3a49ae85862949941fd9ces",
+    img: "/img/stargram.png",
+    explanation:
+      " react, next, typescript를 기반으로 제작한 SNS 웹사이트 입니다.",
+  },
+];
 
 interface Framesprops {
-  setScroll: (value: boolean) => void,
-  q?: THREE.Quaternion,
-  p?: THREE.Vector3,
+  setScroll: (value: boolean) => void;
+  q?: THREE.Quaternion;
+  p?: THREE.Vector3;
 }
 interface Frameprops {
-  url: string,
-  intro: string,
-  href: string,
-  git: string,
-  notion: string,
-  img: string,
-  clicked: MutableRefObject<any>,
-  c?: THREE.Color,
-  explanation: string,
-  objectName: string,
-  setObjectName: (value: string) => void,
+  url: string;
+  intro: string;
+  href: string;
+  git: string;
+  notion: string;
+  img: string;
+  clicked: MutableRefObject<any>;
+  c?: THREE.Color;
+  explanation: string;
+  objectName: string;
+  setObjectName: (value: string) => void;
 }
 
-export default function Frames({ setScroll, q = new THREE.Quaternion(), p = new THREE.Vector3() }: Framesprops) {
+export default function Frames({
+  setScroll,
+  q = new THREE.Quaternion(),
+  p = new THREE.Vector3(),
+}: Framesprops) {
   const ref = useRef<Group>();
   const clicked = useRef<any>();
-  const [objectName, setObjectName] = useState<string>('');
+  const [objectName, setObjectName] = useState<string>("");
 
   useEffect(() => {
-    if (objectName === '') {
+    if (objectName === "") {
       p.set(0, 0, 5.5);
       q.identity();
       return;
@@ -82,12 +89,12 @@ export default function Frames({ setScroll, q = new THREE.Quaternion(), p = new 
       clicked.current.parent.localToWorld(p.set(0, GOLDENRATIO / 2, -0.5));
       clicked.current.parent.getWorldQuaternion(q);
     }
-  })
+  });
 
   useEffect(() => {
     const listener = () => {
       setScroll(true);
-    }
+    };
     return () => {
       window.removeEventListener("wheel", listener);
     };
@@ -98,23 +105,36 @@ export default function Frames({ setScroll, q = new THREE.Quaternion(), p = new 
     state.camera.position.lerp(p, THREE.MathUtils.damp(0, 1, 4, 0.016));
     // 0, 1, 3 => 1(중요), 1, 10
     state.camera.quaternion.slerp(q, THREE.MathUtils.damp(1, 1, 10, 0.016));
-  })
+  });
 
   const onPointerMissed = () => {
-    setObjectName('');
+    setObjectName("");
     clicked.current = undefined;
-  }
+  };
 
   return (
-    <group
-      ref={ref}
-      onPointerMissed={onPointerMissed}>
-      {images.map((props) => <Frame key={props.url} clicked={clicked} setObjectName={setObjectName} objectName={objectName} {...props} /> /* prettier-ignore */)}
+    <group ref={ref} onPointerMissed={onPointerMissed}>
+      {images.map(
+        (props) => <Frame key={props.url} clicked={clicked} setObjectName={setObjectName} objectName={objectName} {...props} /> /* prettier-ignore */
+      )}
     </group>
-  )
+  );
 }
 
-function Frame({ url, c = new THREE.Color(), intro, href, git, notion, img, explanation, clicked, setObjectName, objectName, ...props }: Frameprops) {
+function Frame({
+  url,
+  c = new THREE.Color(),
+  intro,
+  href,
+  git,
+  notion,
+  img,
+  explanation,
+  clicked,
+  setObjectName,
+  objectName,
+  ...props
+}: Frameprops) {
   const [hovered, hover] = useState(false);
   const [object, setObject] = useState<any>();
   const [rnd] = useState(() => Math.random());
@@ -123,11 +143,24 @@ function Frame({ url, c = new THREE.Color(), intro, href, git, notion, img, expl
   const name = getUuid(url);
   useCursor(hovered);
   useFrame((state) => {
-    image.current.material.zoom = hovered ? 0.5 : 0.75 + Math.sin(rnd * 10000 + state.clock.elapsedTime / 3) / 4;
-    image.current.scale.x = THREE.MathUtils.lerp(image.current.scale.x, 0.85 * (hovered ? 0.85 : 1), 0.1);
-    image.current.scale.y = THREE.MathUtils.lerp(image.current.scale.y, 0.9 * (hovered ? 0.905 : 1), 0.1);
-    frame.current!.material.color.lerp(c.set(hovered ? '#C0C0C0' : 'white').convertSRGBToLinear(), 0.1);
-  })
+    image.current.material.zoom = hovered
+      ? 0.5
+      : 0.75 + Math.sin(rnd * 10000 + state.clock.elapsedTime / 3) / 4;
+    image.current.scale.x = THREE.MathUtils.lerp(
+      image.current.scale.x,
+      0.85 * (hovered ? 0.85 : 1),
+      0.1
+    );
+    image.current.scale.y = THREE.MathUtils.lerp(
+      image.current.scale.y,
+      0.9 * (hovered ? 0.905 : 1),
+      0.1
+    );
+    frame.current!.material.color.lerp(
+      c.set(hovered ? "#C0C0C0" : "white").convertSRGBToLinear(),
+      0.1
+    );
+  });
 
   const handleClick = (e: ThreeEvent<MouseEvent>) => {
     e.stopPropagation();
@@ -137,10 +170,10 @@ function Frame({ url, c = new THREE.Color(), intro, href, git, notion, img, expl
     } else {
       setObjectName(e.object.name);
     }
-  }
+  };
   const onClickLink = () => {
-    window.open(href, '_blank');
-  }
+    window.open(href, "_blank");
+  };
   return (
     <group {...props}>
       <mesh
@@ -149,51 +182,85 @@ function Frame({ url, c = new THREE.Color(), intro, href, git, notion, img, expl
         onPointerOver={() => hover(true)}
         onPointerOut={() => hover(false)}
         scale={[GOLDENRATIO, 1, 0.05]}
-        position={[0, GOLDENRATIO / 2, 0]}>
+        position={[0, GOLDENRATIO / 2, 0]}
+      >
         <boxGeometry />
-        <meshStandardMaterial color="#151515" metalness={0.5} roughness={0.5} envMapIntensity={2} />
-        <mesh ref={frame} raycast={() => null} scale={[0.9, 0.93, 0.9]} position={[0, 0, 0.2]} castShadow>
+        <meshStandardMaterial
+          color="#151515"
+          metalness={0.5}
+          roughness={0.5}
+          envMapIntensity={2}
+        />
+        <mesh
+          ref={frame}
+          raycast={() => null}
+          scale={[0.9, 0.93, 0.9]}
+          position={[0, 0, 0.2]}
+          castShadow
+        >
           <boxGeometry />
           <meshBasicMaterial toneMapped={false} fog={false} />
         </mesh>
-        <Image raycast={() => null} ref={image} position={[0, 0, 0.7]} url={url} />
+        <Image
+          raycast={() => null}
+          ref={image}
+          position={[0, 0, 0.7]}
+          url={url}
+        />
       </mesh>
-      <Text maxWidth={0.1} anchorX="left" anchorY="top" position={[0.25, 1.5, 0]} fontSize={0.1} onClick={onClickLink}>
+      <Text
+        maxWidth={0.1}
+        anchorX="left"
+        anchorY="top"
+        position={[0.25, 1.5, 0]}
+        fontSize={0.1}
+        onClick={onClickLink}
+      >
         {intro}
       </Text>
-      {object?.name === objectName &&
+      {object?.name === objectName && (
         <Html center>
           <Container>
             <Explanation>
-              {explanation.split('\n').map((line) => {
+              {explanation.split("\n").map((line) => {
                 return (
                   <span key={line}>
                     {line}
                     <br />
                   </span>
-                )
-              })
-              }
+                );
+              })}
             </Explanation>
-          <SeeMore>
+            <SeeMore>
               <A href={git} target="_blank" rel="noopener noreferrer">
-                <img src={'img/github.png'} alt={'github'} width={32} height={32}/>
+                <img
+                  src={"img/github.png"}
+                  alt={"github"}
+                  width={32}
+                  height={32}
+                />
               </A>
-              <A href={notion} target="_blank" rel="noopener noreferrer">
-                <img src={'img/notion.png'} alt={'notion'} width={32} height={32}/>
-              </A>
-              {
-                img !== '' &&
+              {notion !== "" && (
+                <A href={notion} target="_blank" rel="noopener noreferrer">
+                  <img
+                    src={"img/notion.png"}
+                    alt={"notion"}
+                    width={32}
+                    height={32}
+                  />
+                </A>
+              )}
+              {img !== "" && (
                 <A href={href} target="_blank" rel="noopener noreferrer">
                   <img src={img} alt={intro} width={32} height={32} />
                 </A>
-              }
+              )}
             </SeeMore>
           </Container>
         </Html>
-      }
+      )}
     </group>
-  )
+  );
 }
 
 const Container = styled.div`
@@ -208,19 +275,21 @@ const Container = styled.div`
   border-width: 1px;
   border-color: rgba(238, 237, 242, 0.34);
   border-radius: 0.9em;
-  background-image: linear-gradient(128deg, rgba(0, 0, 0, 0.35), rgba(26, 34, 41, 0.5) 50%);
+  background-image: linear-gradient(
+    128deg,
+    rgba(0, 0, 0, 0.35),
+    rgba(26, 34, 41, 0.5) 50%
+  );
 `;
 
-const Explanation = styled.div`
-
-`;
+const Explanation = styled.div``;
 
 const SeeMore = styled.div`
   margin-top: 4px;
   & > a {
     width: 32px;
     cursor: pointer;
-    background-color: #FFF;
+    background-color: #fff;
     border-radius: 50%;
     margin-right: 12px;
   }
